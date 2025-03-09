fd530da723a6cae690b9efa0cf851750c72d4f11
el-noir <mudasirshah9777@gmail.com>
2025-03-09 22:23:49 +0500

    cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || '*',
+    credentials: true,
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
