29a7c9035c2c0ee00295916bd514c13e970bb0bb
el-noir <mudasirshah9777@gmail.com>
2025-03-10 05:28:38 +0500

    joi - userinput validation middleware

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { getAllUsers, getUserById, createUser, updateUser, deleteUser } from '../controllers/user.controller.js';
+import validateUser from '../middlewares/input.validator.js';
 
 const router = express.Router();
 
-router.get('/', getAllUsers);
+router.get('/',getAllUsers);
 router.get('/:id', getUserById);
-router.post('/', createUser);
-router.put('/:id', updateUser);
+router.post('/',validateUser, createUser);
+router.put('/:id',validateUser, updateUser);
 router.delete('/:id', deleteUser);
 
 export default router;
